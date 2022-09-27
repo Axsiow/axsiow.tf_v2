2de51719a6967fb606410f436bbf8b2076b64693
Enzo Fogliano <TehSiow2@gmail.com>
2022-09-27 20:23:04 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+	<NavBar />
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue';
+export default {
+    name: "DefaultLayout",
+    data() {
+        return {
+            items: [
+                {
+                    title: "Home",
+                    icon: "home",
+                    to: { name: "index" }
+                },
+                {
+                    title: "Inspire",
+                    icon: "lightbulb",
+                    to: { name: "inspire" }
+                }
+            ]
+        };
+    },
+    components: { NavBar }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<NavBar />
+</template>
+
+<script>
+import Card from '~/components/Card'
+import NavBar from '../components/NavBar.vue'
+
+export default {
+	name: 'IndexPage',
+	components: {
+    Card,
+    NavBar
+}
+}
+</script>
