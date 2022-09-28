e524660de4fcc5b4d12b05acdfc5017353771da6
Axsiow <TehSiow2@gmail.com>
2022-09-28 09:58:52 +0200

    error page

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,15 @@
 import Card from '~/components/Card'
 import NuxtLogo from '~/components/NuxtLogo.vue'
 import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
 	name: 'IndexPage',
 	components: {
     Card,
+	NuxtLogo,
     NavBar,
-	NuxtLogo
+	Footer
 	}
 }
 </script>
