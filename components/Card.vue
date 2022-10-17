de554d63baf38bf412e2a21b854546d164950898
Axsiow <TehSiow2@gmail.com>
2022-10-17 20:22:46 +0200

    add certs

--- components/Card.vue
+++ components/Card.vue
@@ -3,14 +3,14 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certTHMCyberDefense.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://tryhackme.com/img/favicon.png" alt="TryHackMe logo">
 						</figure>
 					</div>
 					<div class="media-content">
@@ -25,18 +25,18 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certTHMWebFundamentals.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://tryhackme.com/img/favicon.png" alt="TryHackMe logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
+						<p class="title is-4">Web Fundamentals Hacking</p>
 						<p class="subtitle is-6">@TryHackMe</p>
 						<time datetime="2022-10">Octobre 2022</time>
 					</div>
@@ -46,20 +46,20 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certTHMPentest+.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://tryhackme.com/img/favicon.png" alt="TryHackMe logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
+						<p class="title is-4">CompTIA Pentest+</p>
 						<p class="subtitle is-6">@TryHackMe</p>
-						<time datetime="2022-10">Octobre 2022</time>
+						<time datetime="2022-8">Août 2022</time>
 					</div>
 				</div>
   			</div>
@@ -67,20 +67,20 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certPythonDataScience.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
-						<p class="subtitle is-6">@TryHackMe</p>
-						<time datetime="2022-10">Octobre 2022</time>
+						<p class="title is-4">Python Data Science</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2022-8">Août 2022</time>
 					</div>
 				</div>
   			</div>
@@ -88,20 +88,20 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certPythonCore.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
-						<p class="subtitle is-6">@TryHackMe</p>
-						<time datetime="2022-10">Octobre 2022</time>
+						<p class="title is-4">Python Core</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2022-7">Juillet 2022</time>
 					</div>
 				</div>
   			</div>
@@ -109,20 +109,20 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certPythonDataStructures.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
-						<p class="subtitle is-6">@TryHackMe</p>
-						<time datetime="2022-10">Octobre 2022</time>
+						<p class="title is-4">Python Data Structure</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2022-7">Juillet 2022</time>
 					</div>
 				</div>
   			</div>
@@ -130,20 +130,20 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certPythonIntermediate.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
-						<p class="subtitle is-6">@TryHackMe</p>
-						<time datetime="2022-10">Octobre 2022</time>
+						<p class="title is-4">Python Intermediate</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2022-7">Juillet 2022</time>
 					</div>
 				</div>
   			</div>
@@ -151,24 +151,193 @@
 		<div class="card">
 			<div class="card-image">
 				<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				<img src="../assets/certifications/certWebDev.png" alt="Certification proof">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
 					<div class="media-left">
 						<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">Cyber Defense</p>
-						<p class="subtitle is-6">@TryHackMe</p>
-						<time datetime="2022-10">Octobre 2022</time>
+						<p class="title is-4">Web Development</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2021-7">Juillet 2022</time>
 					</div>
 				</div>
   			</div>
 		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certCiscoCybersecurity2.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Cisco_academy_logo.svg/1200px-Cisco_academy_logo.svg.png" alt="Cisco Networking Academy logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cybersecurity Essentials</p>
+						<p class="subtitle is-6">@Cisco Networking Academy</p>
+						<time datetime="2022-6">Juin 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certCiscoLinux.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Cisco_academy_logo.svg/1200px-Cisco_academy_logo.svg.png" alt="Cisco Networking Academy logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">NDG Linux Essentials</p>
+						<p class="subtitle is-6">@Cisco Networking Academy</p>
+						<time datetime="2021-7">Juin 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certSNA.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://upload.wikimedia.org/wikipedia/fr/archive/d/d9/20190420123211%21ANSSI_Logo.svg" alt="ANSSI logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">SecNumAcadémie</p>
+						<p class="subtitle is-6">@ANSSI</p>
+						<time datetime="2021-7">Juillet 2021</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certJava.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Java</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2021-4">Avril 2021</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certJQuery.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">JQuery</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2021-11">Novembre 2021</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certJS.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Javascript</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2019-10">Octobre 2019</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certCSS.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">CSS</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2019-10">Octobre 2019</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="../assets/certifications/certHTML.png" alt="Certification proof">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://blob.sololearn.com/avatars/sololearn.png" alt="Sololearn logo">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">HTML</p>
+						<p class="subtitle is-6">@Sololearn</p>
+						<time datetime="2019-10">Octobre 2019</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		
 		
     </div>
 </template>
@@ -188,6 +357,8 @@
 
 .card {
 	margin-top: 25px;
+	width: 400px;
+	height: 450px;
 }
 
 .media-content {
