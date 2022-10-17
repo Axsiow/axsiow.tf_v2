f29e452646b5b16677439b5e86abd66b039ba50e
Axsiow <TehSiow2@gmail.com>
2022-10-17 16:37:38 +0200

    Update Card.vue

--- components/Card.vue
+++ components/Card.vue
@@ -14,12 +14,162 @@
 						</figure>
 					</div>
 					<div class="media-content">
-						<p class="title is-4">John Smith</p>
-						<p class="subtitle is-6">Octobre 2022</p>
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
 					</div>
 				</div>
   			</div>
 		</div>
+		
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		<div class="card">
+			<div class="card-image">
+				<figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				<div class="media">
+					<div class="media-left">
+						<figure class="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="media-content">
+						<p class="title is-4">Cyber Defense</p>
+						<p class="subtitle is-6">@TryHackMe</p>
+						<time datetime="2022-10">Octobre 2022</time>
+					</div>
+				</div>
+  			</div>
+		</div>
+		
     </div>
 </template>
 
@@ -35,9 +185,19 @@
 	align-items: center;
 	align-content: center;
 }
+
 .card {
-	min-width: 400px;
-	max-height: 400px;
-	max-width: 400px;
+	margin-top: 25px;
+}
+
+.media-content {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: flex-start;
+}
+
+time {
+	align-self: flex-end;
 }
 </style>
